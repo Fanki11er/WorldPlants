ca7a0d576ad89f94acc4397656bcaf31c5687bf7
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2023-03-18 19:31:16 +0100

    Added folder which will contain plants images to gitignore file. Added basic User Table and UserSettingsTable

---
+++ WorldPlants/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WorldPlants.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public string AccountType { get; set; }
+        public Guid SpaceId { get; set; }
+
+    }
+}
---
+++ WorldPlants/Entities/UserSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace WorldPlants.Entities
+{
+    public class UserSettings
+    {
+        public Guid Id { get; set; }
+        public bool ReceiveEmails { get; set; }
+        public bool CanWaterPlants { get; set; }
+        public bool CanMistPlants { get; set; }
+        public bool CanFertilizePlants { get; set; }
+        public bool CanRepotPlants { get; set; }
+        public bool CanMovePlants { get; set; }
+        public bool AddPlants { get; set; }
+        public bool RemovePlants { get; set;}
+        public bool EditPlants { get; set; }
+
+    }
+}
