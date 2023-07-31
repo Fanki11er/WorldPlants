b86a1ebb3df0381d4927002657d97e4d9c6d1aa6
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2023-07-31 22:53:42 +0200

    Added Email to SanitizedGuestUserDto

--- WorldPlants/Models/SanitizedGuestUserDto.cs
+++ WorldPlants/Models/SanitizedGuestUserDto.cs
@@ -1,9 +1,12 @@
-﻿namespace WorldPlants.Models
+﻿// Ignore Spelling: Dto
+
+namespace WorldPlants.Models
 {
     public class SanitizedGuestUserDto
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
         public bool IsActive { get; set; }
     }
 }
