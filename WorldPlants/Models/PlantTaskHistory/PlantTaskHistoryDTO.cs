cf3fe857c64ee9795a107f60f5ba35736412204f
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2023-10-30 22:20:34 +0100

    Fixed missing field in TasksHistoty

--- WorldPlants/Models/PlantTaskHistory/PlantTaskHistoryDTO.cs
+++ WorldPlants/Models/PlantTaskHistory/PlantTaskHistoryDTO.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string TaskType { get; set; }
+        public string TaskName { get; set; }
         public string UserName { get; set; }
         public string ExecutionDate { get; set; }
     }
